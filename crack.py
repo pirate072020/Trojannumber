6ffdb619745c3ddcdc582ccb3099b400b5488075
Adham <56234736+pirate072020@users.noreply.github.com>
2020-03-04 13:49:11 +0700

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,25 @@
+# - *- coding: utf- 8 - *
+import time
+
+
+for i in range(10):
+    time.sleep(0.5)
+    print "\033[91mLoading: " + str(i +1)
+print ("\033[91m88P'\`888`Y88                  ,e,                   888")
+print ("\033[91mP`  888  `Y 888,8,  e88 88e   `   ,`Y88b 888 8e    888 ee Y8b Y8b Y888P")
+print ("\033[91m    888     888 `  d888 888b 888 `8` 888 888 88b   888 P   Y8b Y8b Y8P")
+print ("\033[91m    888     888    Y888 888P 888 ,ee 888 888 888   888 b    Y8b Y8b")
+print ("\033[91m    888     888     *88 88*  888 *88 888 888 888   888 8b    YP  Y8P")
+print ("\033[91m                             88p")
+print ("\033[91m                             8`")
+a = raw_input ("\033[91mmasukan no korban : ")
+
+print("\033[91mmengatack no korban.")
+time.sleep(2.4)
+print("wait after 2.4 seconds.")
+time.sleep(4.5)
+print("\033[92mcracking ... ")
+
+p =10000000
+for i  in range (p) :
+      print "\033[91mattack !  :"+str(i)
